749d447fd98110370725be1ccbc0c9e1a3e26d15
imran <imohammed23@gmail.com>
2016-06-01 20:11:45 +0530

    upgrade dependency versions

--- eve-stores/sql/src/main/java/uncmn/eve/store/sql/SqlStore.java
+++ eve-stores/sql/src/main/java/uncmn/eve/store/sql/SqlStore.java
@@ -17,98 +17,88 @@ import uncmn.eve.Value;
  */
 public class SqlStore extends Store {
 
-    BriteDatabase db;
+  BriteDatabase db;
 
-    SqlStore(Context context, Converter converter) {
-        super(converter);
+  SqlStore(Context context, Converter converter) {
+    super(converter);
 
-        SqlStoreOpenHelper openHelper = new SqlStoreOpenHelper(context);
+    SqlStoreOpenHelper openHelper = new SqlStoreOpenHelper(context);
 
-        db = SqlBrite.create(new SqlBrite.Logger() {
-            @Override
-            public void log(String message) {
+    db = SqlBrite.create(new SqlBrite.Logger() {
+      @Override public void log(String message) {
 
-            }
-        }).wrapDatabaseHelper(openHelper, Schedulers.io());
+      }
+    }).wrapDatabaseHelper(openHelper, Schedulers.io());
+  }
+
+  public static SqlStore create(Context context, Converter converter) {
+    return new SqlStore(context, converter);
+  }
+
+  @Override public void set(String key, Value value) {
+    ValueQuery query = ValueQuery.queryBuilder().key(key);
+
+    final String sql = query.sql();
+    final String[] args = query.args();
+
+    Cursor cursor = db.query(sql, args);
+    ContentValues contentValues = ValueQuery.contentValues(key, value);
+
+    if (cursor != null && cursor.getCount() > 0) {
+      db.update(ValueQuery.TABLE, contentValues, SQLiteDatabase.CONFLICT_REPLACE,
+          ValueQuery.WHERE_KEY, args);
+    } else {
+      db.insert(ValueQuery.TABLE, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
     }
+  }
 
-    public static SqlStore create(Context context, Converter converter) {
-        return new SqlStore(context, converter);
-    }
+  @Override public <T> T get(String key) {
+    ValueQuery query = ValueQuery.queryBuilder().key(key);
+    final String sql = query.sql();
+    final String[] args = query.args();
 
-    @Override
-    public void set(String key, Value value) {
-        ValueQuery query = ValueQuery.queryBuilder()
-            .key(key);
+    Cursor cursor = db.query(sql, args);
+    if (cursor != null && cursor.getCount() > 0) {
+      cursor.moveToFirst();
+      boolean isPrimitive =
+          cursor.getInt(cursor.getColumnIndexOrThrow(ValueQuery.IS_PRIMITIVE)) == 1;
+      String c = cursor.getString(cursor.getColumnIndexOrThrow(ValueQuery.TYPE));
 
-        final String sql = query.sql();
-        final String[] args = query.args();
+      Class<?> type = Types.getClassType(c, isPrimitive);
 
-        Cursor cursor = db.query(sql, args);
-        ContentValues contentValues = ValueQuery.contentValues(key, value);
-
-        if (cursor != null && cursor.getCount() > 0) {
-            db.update(ValueQuery.TABLE, contentValues, SQLiteDatabase.CONFLICT_REPLACE, ValueQuery.WHERE_KEY, args);
-        } else {
-            db.insert(ValueQuery.TABLE, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
-        }
-    }
-
-    @Override
-    public <T> T get(String key) {
-        ValueQuery query = ValueQuery
-            .queryBuilder()
-            .key(key);
-
-        final String sql = query.sql();
-        final String[] args = query.args();
-
-        Cursor cursor = db.query(sql, args);
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            boolean isPrimitive = cursor.getInt(cursor.getColumnIndexOrThrow(ValueQuery.IS_PRIMITIVE)) == 1;
-            String c = cursor.getString(cursor.getColumnIndexOrThrow(ValueQuery.TYPE));
-
-            Class<?> type = Types.getClassType(c, isPrimitive);
-
-            if (type == null) {
-                return null;
-            }
-
-            byte[] value = cursor.getBlob(cursor.getColumnIndexOrThrow(ValueQuery.VALUE));
-            return convert(value, type);
-
-        }
+      if (type == null) {
         return null;
+      }
+
+      byte[] value = cursor.getBlob(cursor.getColumnIndexOrThrow(ValueQuery.VALUE));
+      return convert(value, type);
     }
+    return null;
+  }
 
-    @Override
-    public boolean delete(String key) {
-        ValueQuery query = ValueQuery.queryBuilder()
-            .key(key);
+  @Override public boolean delete(String key) {
+    ValueQuery query = ValueQuery.queryBuilder().key(key);
 
-        final String sql = query.sql();
-        final String[] args = query.args();
-        Cursor cursor = db.query(sql, args);
-        if (cursor != null && cursor.getCount() > 0) {
-            db.delete(ValueQuery.TABLE, ValueQuery.WHERE_KEY, args);
-            return true;
-        }
-        return false;
+    final String sql = query.sql();
+    final String[] args = query.args();
+    Cursor cursor = db.query(sql, args);
+    if (cursor != null && cursor.getCount() > 0) {
+      db.delete(ValueQuery.TABLE, ValueQuery.WHERE_KEY, args);
+      return true;
     }
+    return false;
+  }
 
-    @Override
-    public boolean exists(String key) {
-        ValueQuery query = ValueQuery.queryBuilder()
-            .key(key);
+  @Override public boolean exists(String key) {
+    ValueQuery query = ValueQuery.queryBuilder().key(key);
 
-        final String sql = query.sql();
-        final String[] args = query.args();
-        Cursor cursor = db.query(sql, args);
-        if (cursor != null && cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            return true;
-        }
-        return false;
+    final String sql = query.sql();
+    final String[] args = query.args();
+    Cursor cursor = db.query(sql, args);
+    if (cursor != null && cursor.getCount() > 0) {
+      cursor.moveToFirst();
+      return true;
     }
+    return false;
+  }
 }
